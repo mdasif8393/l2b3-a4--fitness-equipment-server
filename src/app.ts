aebb02cc25e9e22d71ba56275a0e32ce39a0dc62
mdasif8393 <md.asif.8393@gmail.com>
2024-07-09 12:41:03 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+// use all route
+// app.use("/api/products", ProductRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "l2b3 Assignment: 4 Fitness Equipment Server Running",
+  });
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+export default app;
